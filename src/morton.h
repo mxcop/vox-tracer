9b3b21d76d3f1fbfd85ff30d10ce720501a4e16c
mxcop <mx.coppen@gmail.com>
2024-02-27 21:40:17 +0100

    feat: fast morton encode for SIMD packet

--- src/morton.h
+++ src/morton.h
@@ -15,38 +15,50 @@ inline u64 morton_encode(const u32 x, const u32 y, const u32 z) {
            _pdep_u64((u64)z, BMI_3D_Z_MASK);
 }
 
-/* Fast version of morton encode for SSE */
-//inline i128 morton_encode(const i128 x, const i128 y, const i128 z) {
-//    constexpr u64 X_MASK = 0x92492492;
-//    constexpr u64 Y_MASK = 0x24924924;
-//    constexpr u64 Z_MASK = 0x49249249;
-//
-//    /* Gather lower and upper 64 bit integers */
-//    const u64 x1 = _mm_cvtsi128_si64(x);
-//    const u64 y1 = _mm_cvtsi128_si64(y);
-//    const u64 z1 = _mm_cvtsi128_si64(z);
-//    const u64 x2 = _mm_extract_epi64(x, 1);
-//    const u64 y2 = _mm_extract_epi64(y, 1);
-//    const u64 z2 = _mm_extract_epi64(z, 1);
-//
-//    /* Perform parallel bit deposits */
-//    const u64 p1 = _pdep_u64(x1, X_MASK);
-//    const u64 p2 = _pdep_u64(x2, X_MASK);
-//    const u64 p3 = _pdep_u64(y1, Y_MASK);
-//    const u64 p4 = _pdep_u64(y2, Y_MASK);
-//    const u64 p5 = _pdep_u64(z1, Z_MASK);
-//    const u64 p6 = _pdep_u64(z2, Z_MASK);
-//
-//    // x | y | z
-//    /* Combine the results */
-//    const u64 m135 = p1 | p3 | p5;
-//    const u64 m246 = p2 | p4 | p6;
-//
-//    /* Store and return the resulting vector */
-//    return _mm_set_epi64x(m246, m135);
-//    //_mm_insert_epi64(_mm_cvtsi64_si128(m135), m246, 1);
-//}
+/* Fast version of morton encode for SSE using BMI2 (limited to 11/10 bits per axis) */
 inline i128 morton_encode(const i128 x, const i128 y, const i128 z) {
+    /* Bit deposit masks */
+    constexpr u64 X_MASK = 0x4924924949249249; /* 11 bits */
+    constexpr u64 Y_MASK = 0x9249249292492492; /* 11 bits */
+    constexpr u64 Z_MASK = 0x2492492424924924; /* 10 bits */
+
+    /* Adjust bit format for conversion */
+    const i128 sx = _mm_srli_epi64(x, (32 - 11 /* bits */));
+    const i128 sy = _mm_srli_epi64(y, (32 - 11 /* bits */));
+    const i128 sz = _mm_srli_epi64(z, (32 - 10 /* bits */));
+    const i128 b11m = _mm_set1_epi64x(0b11111111111);
+    const i128 b10m = _mm_set1_epi64x(0b01111111111);
+    /* TIP: technically I could 'not' do the 'andnot' it will only fail with very large numbers */
+    const i128 cx = _mm_or_si128(_mm_andnot_si128(b11m, sx), _mm_and_si128(b11m, x));
+    const i128 cy = _mm_or_si128(_mm_andnot_si128(b11m, sy), _mm_and_si128(b11m, y));
+    const i128 cz = _mm_or_si128(_mm_andnot_si128(b10m, sz), _mm_and_si128(b10m, z));
+
+    /* Gather lower and upper 64 bit integers */
+    const u64 x1 = _mm_cvtsi128_si64(cx);
+    const u64 y1 = _mm_cvtsi128_si64(cy);
+    const u64 z1 = _mm_cvtsi128_si64(cz);
+    const u64 x2 = _mm_extract_epi64(cx, 1);
+    const u64 y2 = _mm_extract_epi64(cy, 1);
+    const u64 z2 = _mm_extract_epi64(cz, 1);
+
+    /* Perform parallel bit deposits */
+    const u64 p1 = _pdep_u64(x1, X_MASK);
+    const u64 p2 = _pdep_u64(x2, X_MASK);
+    const u64 p3 = _pdep_u64(y1, Y_MASK);
+    const u64 p4 = _pdep_u64(y2, Y_MASK);
+    const u64 p5 = _pdep_u64(z1, Z_MASK);
+    const u64 p6 = _pdep_u64(z2, Z_MASK);
+
+    // x | y | z
+    /* Combine the results */
+    const u64 m135 = p1 | p3 | p5;
+    const u64 m246 = p2 | p4 | p6;
+
+    /* Store and return the resulting vector */
+    return _mm_set_epi64x(m246, m135);
+}
+
+inline i128 morton_encode_ex(const i128 x, const i128 y, const i128 z) {
     return _mm_set_epi32(morton_encode(x.m128i_u32[3], y.m128i_u32[3], z.m128i_u32[3]),
                          morton_encode(x.m128i_u32[2], y.m128i_u32[2], z.m128i_u32[2]),
                          morton_encode(x.m128i_u32[1], y.m128i_u32[1], z.m128i_u32[1]),
